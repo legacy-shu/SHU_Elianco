39cdfc933e4202fbc35d24896023454cd59f24c3
Ali56300 <75322529+Ali56300@users.noreply.github.com>
2022-03-30 13:49:41 +0100

    Sticky navbar

--- ElancoGroupB/Models/Item.cs
+++ ElancoGroupB/Models/Item.cs
@@ -5,5 +5,5 @@ public class Item
     public Dictionary<string, string>? Description { get; set; }
     public Dictionary<string, string>? Quantity { get; set; }
     public Dictionary<string, string>? Amount { get; set; }
-
+    public Dictionary<string, string>? Accuracy { get; set; }
 }
