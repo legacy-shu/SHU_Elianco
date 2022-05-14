1d1a14d5cf5d26736eaf4ea51dc7fdfe515d6d09
w.ryan.jung <w.ryan.jung@gmail.com>
2022-05-14 19:20:07 +0200

    update

--- ElancoGroupB/Program.cs
+++ ElancoGroupB/Program.cs
@@ -1,5 +1,7 @@
+using System.Net;
 using AspNetCoreHero.ToastNotification;
 using ElancoGroupB.Models;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,13 @@ builder.Services.AddNotyf(config =>
     config.IsDismissable = true;
     config.Position = NotyfPosition.TopRight;
 });
+
+// using System.Net;
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.KnownProxies.Add(IPAddress.Parse("lundaljung.com"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,9 +28,17 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+// using Microsoft.AspNetCore.HttpOverrides;
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+app.UseAuthentication();
+
 app.UseStaticFiles();
 
 app.UseRouting();
