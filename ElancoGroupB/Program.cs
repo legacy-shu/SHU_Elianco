4ef3221cdd52efb8cb7cbffd1099cb59c2a2aca9
w-ryan-jung <w.ryan.jung@gmail.com>
2022-05-16 21:57:52 +0200

    update

--- ElancoGroupB/Program.cs
+++ ElancoGroupB/Program.cs
@@ -25,14 +25,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    //app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-    app.UseHttpsRedirection();
+    //app.UseHsts();
+    //app.UseHttpsRedirection();
 }
 else
 {
-    app.UseDeveloperExceptionPage();
+    //app.UseDeveloperExceptionPage();
 }
 
 // using Microsoft.AspNetCore.HttpOverrides;
